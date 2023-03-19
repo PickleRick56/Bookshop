989e89511d04229f5a845ef1f5f18df0b54f7ce6
PickleRick56 <36300782+PickleRick56@users.noreply.github.com>
2023-03-19 17:58:22 +0500

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,137 @@
+document.addEventListener("DOMContentLoaded", () => {
+  sliderStarter();
+});
+
+function sliderStarter() {
+  setInterval(() => forward(), 5000);
+}
+
+let position = 0;
+
+let slider = document.querySelector(".slider");
+let dots = document.querySelector(".dots");
+let allDots = dots.children;
+let cards = document.querySelector(".cards");
+
+const imageCollection = [
+  "img/banner0.png",
+  "img/banner1.png",
+  "img/banner2.png",
+];
+
+slider.style.backgroundImage = "url('img/banner0.png')";
+
+function forward() {
+  position += 1;
+
+  if (position > imageCollection.length - 1) {
+    position = 0;
+    slider.style.backgroundImage = `url(${imageCollection[position]})`;
+  } else {
+    slider.style.backgroundImage = `url(${imageCollection[position]})`;
+  }
+  for (let key of allDots) {
+    key.style.backgroundColor = "#EFEEF6";
+  }
+  document.querySelector(`.dot${position}`).style.backgroundColor = "#9E98DC";
+}
+
+function directSelection(evt) {
+  let index = evt.target.className;
+  index = index.slice(-1);
+  position = index;
+  for (let key of allDots) {
+    key.style.backgroundColor = "#EFEEF6";
+  }
+  slider.style.backgroundImage = `url(${imageCollection[position]})`;
+  document.querySelector(`.dot${position}`).style.backgroundColor = "#9E98DC";
+}
+
+dots.addEventListener("click", directSelection, false);
+
+// BOOKS
+(function () {
+  fetch(
+    'https://www.googleapis.com/books/v1/volumes?q="subject:Humor"&key=AIzaSyARaQbqJaGTu2k41QqIQHeM5DIhY69brqs&printType=books&startIndex=0&maxResults=6&langRestrict=en'
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+
+      for (let i = 0; i < data.items.length; i++) {
+        if (!data.items[i].saleInfo.retailPrice) {
+          cards.innerHTML += `   <div class="card">
+          <img class="card_cover" src="${
+            data.items[i].volumeInfo.imageLinks.thumbnail
+          }" alt="">
+          <div class="description">
+  
+              <div class="author">${data.items[i].volumeInfo.authors}</div>
+              <div class="title">${data.items[i].volumeInfo.title}</div>
+              <div class="ratingsCount">${
+                data.items[i].volumeInfo.ratingsCount
+              } &#9734;</div>
+              <div class="review">${
+                data.items[i].volumeInfo.description.length > 155
+                  ? data.items[i].volumeInfo.description.slice(0, 155) + "..."
+                  : data.items[i].volumeInfo.description
+              }</div>
+
+              <div class="price">${data.items[i].saleInfo.saleability.replace(
+                /_/g,
+                " "
+              )}</div>
+  
+              
+              
+          </div>
+  
+      </div>`;
+        } else {
+          cards.innerHTML += `   <div class="card">
+          <img class="card_cover" src="${
+            data.items[i].volumeInfo.imageLinks.thumbnail
+          }" alt="">
+          <div class="description">
+  
+              <div class="author">${data.items[i].volumeInfo.authors}</div>
+              <div class="title">${data.items[i].volumeInfo.title}</div>
+              <div class="ratingsCount">${
+                data.items[i].volumeInfo.ratingsCount
+              } &#9734;</div>
+              <div class="review">${
+                data.items[i].volumeInfo.description.length > 155
+                  ? data.items[i].volumeInfo.description.slice(0, 155) + "..."
+                  : data.items[i].volumeInfo.description
+              }</div>
+  
+              
+              <div class="price">${data.items[i].saleInfo.retailPrice.amount}${
+            data.items[i].saleInfo.retailPrice.currencyCode
+          }</div>
+          </div>
+  
+      </div>`;
+        }
+      }
+
+      // cards.innerHTML += `   <div class="card">
+      //     <img class="card_cover" src="${data.items[4].volumeInfo.imageLinks.thumbnail}" alt="">
+      //     <div class="description">
+
+      //         <div class="author">${data.items[4].volumeInfo.authors}</div>
+      //         <div class="title">${data.items[4].volumeInfo.title}</div>
+      //         <div class="ratingsCount">${data.items[4].volumeInfo.ratingsCount} &#9734;</div>
+      //         <div class="review">${data.items[4].volumeInfo.description}</div>
+      //         <div class="price">${data.items[4].saleInfo.retailPrice.amount}  ${data.items[4].saleInfo.retailPrice.currencyCode}</div>
+      //     </div>
+
+      // </div>`;
+    })
+
+    .catch(() => {
+      console.log("error");
+    });
+})();
